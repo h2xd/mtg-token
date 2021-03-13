f2e4d6451fa69c1410631659435cd4bfb16c9c5f
Andreas Schujkow <andreas.schujkow@sumcumo.com>
2021-03-13 16:00:16 +0100

    feat(life-counter): add life counter

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,18 @@ export default defineComponent({
 </script>
 
 <style>
+
+:root {
+  --mana-color-red-backgrond: #f51b07;
+  --mana-color-red-text: #350600;
+}
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/LifePointsCounter.vue
+++ src/components/LifePointsCounter.vue
@@ -1,9 +1,10 @@
 <template>
-  <div>{{ MainStore.lifepoints }}</div>
-  <div>{{ MainStore.alive }}</div>
+  <div class="container">
+    <div class="counter">{{ MainStore.lifepoints }}</div>
 
-  <button @click="MainStore.raise(1)">Plus</button>
-  <button @click="MainStore.reduce(1)">Minus</button>
+    <button @click="MainStore.raise(1)" class="raise"></button>
+    <button @click="MainStore.reduce(1)" class="reduce"></button>
+  </div>
 </template>
 
 <script lang="ts">
@@ -19,4 +20,56 @@ export default defineComponent({
   }
 })
 </script>
+
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  background-color: var(--mana-color-red-backgrond);
+  color: var(--mana-color-red-text);
+  text-align: center;
+  width: 100%;
+  height: 100vw;
+}
+
+.counter {
+  font-size: 50vw;
+}
+
+.raise, .reduce {
+  position: absolute;
+  left: 0;
+  width: 100%;
+  height: 25%;
+
+  background-color: transparent;
+  border: 0;
+  outline: 0;
+}
+
+.raise::before, .reduce::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.2));
+}
+
+.raise {
+  top: 0;
+}
+
+.raise::before {
+  transform: rotate(180deg);
+}
+
+.reduce {
+  bottom: 0;
+}
+</style>
 
