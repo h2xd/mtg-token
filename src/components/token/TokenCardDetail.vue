139319db963f42b5c0a360ad20c54209828ae618
Andreas Schujkow <andreas.schujkow@sumcumo.com>
2021-03-22 00:19:17 +0100

    feat(icon): add icon animation

--- src/components/token/TokenCardDetail.vue
+++ src/components/token/TokenCardDetail.vue
@@ -1,14 +1,14 @@
 <template>
-  <component :is="is" class="card" v-if="token">
+  <component :is="is" :class="['card', token.isTapped && 'tapped']" v-if="token">
     <div>{{ token.power }} / {{ token.toughness }}</div>
 
     <div>
-      <CornerDownLeftIcon class="icon" v-if="token.isTapped" />
-      <MoonIcon class="icon" v-if="token.hasSummoningSickness" />
+      <Icon :active="token.isTapped"><CornerDownLeftIcon /></Icon>
+      <Icon :active="token.hasSummoningSickness"><MoonIcon /></Icon>
     </div>
 
-    <button @click="$emit('death', token)">Kill</button>
-    <button @click="$emit('copy', token)">Clone</button>
+    <!--    <button @click="$emit('death', token)">Kill</button>-->
+    <!--    <button @click="$emit('copy', token)">Clone</button>-->
   </component>
 </template>
 
@@ -16,12 +16,14 @@
 import { defineComponent, PropType } from "vue"
 import { TokenCreature } from "../../entities/TokenCreature"
 
+import Icon from "../base/Icon.vue"
 import CornerDownLeftIcon from "../../assets/svg/corner-down-left.svg"
 import MoonIcon from "../../assets/svg/moon.svg"
 
 export default defineComponent({
   name: "TokenCardDetail",
   components: {
+    Icon,
     CornerDownLeftIcon,
     MoonIcon,
   },
@@ -40,13 +42,13 @@ export default defineComponent({
   padding: var(--space-m);
   border: var(--border-width) solid #000;
   border-radius: var(--border-radius);
+  margin: var(--space-m);
+  transition: var(--transition);
 }
 
-.icon {
-  width: 1rem;
-  height: 1rem;
-  stroke-dasharray: 70;
-  stroke-dashoffset: 70;
-  animation: iconKeyFrames 2s ease-in-out 0s 1 forwards;
+.tapped {
+  transform: rotate(5deg);
+  opacity: 0.5;
+  filter: grayscale(1);
 }
 </style>
